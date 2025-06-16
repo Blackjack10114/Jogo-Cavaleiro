bf65aa162f70d148c6b19b640e602bbeabbbc60f
Blackjack10114 <gsl10114a@gmail.com>
2025-06-16 14:40:06 -0300

    Movimento e camera basicos

---
+++ Jogo-Cavaleiro/Assets/Scripts/camera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    GameObject Player;
+    Vector3 offset, posicaocamera;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posicaocamera = new Vector3(transform.position.x, Player.transform.position.y, transform.position.z);
+        this.transform.position = posicaocamera;
+    }
+}
