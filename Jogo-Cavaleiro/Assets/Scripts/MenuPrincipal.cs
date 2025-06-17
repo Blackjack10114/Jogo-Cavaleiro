7d09984542db4759be5fde574044d2ccaeac27b8
Blackjack10114 <gsl10114a@gmail.com>
2025-06-17 15:02:29 -0300

    Menu principal funcional

--- Jogo-Cavaleiro/Assets/Scripts/MenuPrincipal.cs
+++ Jogo-Cavaleiro/Assets/Scripts/MenuPrincipal.cs
@@ -5,11 +5,23 @@ using UnityEngine.SceneManagement;
 public class MenuPrincipal : MonoBehaviour
 {
     public string CenaJogar;
+    public string CenaCredito;
+    public string CenaMenu;
 
     public void Jogar()
     {
         SceneManager.LoadScene(CenaJogar);
     }
-
-
+    public void creditos()
+    {
+        SceneManager.LoadScene(CenaCredito);
+    }
+    public void voltarmenu()
+    {
+        SceneManager.LoadScene(CenaMenu);
+    }
+    public void sair()
+    {
+        Application.Quit();
+    }
 }
